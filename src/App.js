e3bf9fedc08ee23a6bcfc1c7096f5117f6038256
SaheelSingh <saheelsingh1904@gmail.com>
2024-08-14 16:31:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [endpoint, setEndPoint] = useState('')
+  const [container, setContainer] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  async function fetchME() {
+    setLoading(true)
+    const url = `https://imdb8.p.rapidapi.com/auto-complete?q=${endpoint}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd3c03b116fmshf511e570a68ddbep1153d3jsnc50d7676ac00',
+        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+      }
+    }
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setLoading(false)
+      setContainer(data.d)
+      console.log(data.d)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    fetchME()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <input value={endpoint} onChange={(e) => setEndPoint(e.target.value)} type='text' />
+        <button>Submit</button>
+      </form>
+
+      {
+        loading === true && (
+          <span>Loading...</span>
+        )
+      }
+
+      {
+        !loading && container.map(movie => {
+          return (
+            <div key={movie ? movie.id : ""}>
+              <img src={movie ? movie.i.imageUrl : ""} alt='movie_image' />
+              <p>{movie ? movie.l : ""}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
